54a3069c09b1011b93a10e93fc7fe7792b9e8ed4
geogardow <85602062+geogardow@users.noreply.github.com>
2023-02-27 21:34:48 -0600

    Medio sumador
    Se programó un medio sumador en VHDL

---
+++ Parte_2/medio_sumador.vhd
@@ -0,0 +1,15 @@
+entity medio_sumador is
+		port(
+				x : in std_logic;
+				y : in std_logic;
+				s : out std_logic;
+				c : out std_logic
+				);
+end entity medio_sumador;
+
+architecture medio_sumador_logica of medio_sumador is
+	begin 
+		s <= x XOR y;
+		c <= x AND y;
+end medio_sumador_logica;
+

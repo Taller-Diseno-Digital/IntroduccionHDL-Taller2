c9f1d4df0575e4f3d937cca1713dae7d1198e4ca
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2023-03-17 22:12:53 -0600

    delete unused files

--- Laboratorio_3/Problema_1/or_xor_sum/mod_compare.sv
+++
@@ -1,9 +0,0 @@
-module mod_compare #(parameter width = 4)(
-    input logic [width - 1 : 0] a,b,
-	input logic sel,
-    output logic negative,zero
-);
-
-	assign zero = sel ? (a == b) : 0;
-	assign negative = sel ? (a < b):0;
-endmodule
--- Laboratorio_3/Problema_1/or_xor_sum/mod_mux4.sv
+++
@@ -1,7 +0,0 @@
-module mod_mux4 #(parameter width = 4)(input logic [width-1:0] d0, d1, d2, d3,
-    input logic [1:0] s,
-    output logic [3:0] y);
-
-    assign y = s[1] ? (s[0] ? d3 : d2)
-    : (s[0] ? d1 : d0);
-endmodule

ca8182602bc7976cd8a0ed2c9392644ad096e70d
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2023-03-21 09:54:57 -0600

    alu functionality without display 7 seg

--- Laboratorio_3/Problema_1/alu/tb_mod_alu.sv
+++ Laboratorio_3/Problema_1/alu/tb_mod_alu.sv
@@ -13,156 +13,156 @@ module tb_mod_alu();
         cout = 0;
         zero=0;
         negative = 0;
+		  #100;
 		  assert (result == 4'b0000) 
         else   $error("Failed case #1");
-		  #100;
 		  
 		  //---suma---
         a = 4'b1110;
         b = 4'b0101;
         sel = 0; 
+        #100;
 		  assert (result == 3) 
         else   $error("Failed case 14 + 5");
-        #100;
 		  
 		  a = 4'b0010;
         b = 4'b0101;
         sel = 0; 
+        #50;
 		  assert (result === 4'b0111) 
         else   $error("Failed case 2 + 5");
-        #50;
 
 		  //---resta---
         a = 4'b0100;
         b = 4'b0011;
         sel = 1; 
+        #50;
 		  assert (result === 4'b0001 & cout === 1) 
         else   $error("Failed case 4 - 3");
-        #50;
 
         a = 4'b0110;
         b = 4'b1010;
         sel = 1;
-		  assert (result === 4'b0100 & cout === 1 & negative === 1) 
-        else   $error("Failed case 6 - 10");
         #50;
+		  assert (result === 4'b0100 & cout === 0 & negative === 1) 
+        else   $error("Failed case 6 - 10");
 		  
         a = 4'b0110;
         b = 4'b0110;
         sel = 1;
+        #50;
 		  assert (result === 4'b0000 & cout === 1) 
         else   $error("Failed case 6 - 6");
-        #50;
 
 		  // ---not a---
         a = 4'b1011;
         sel = 2; 
+        #50;
 		  assert (result === 4'b0100 & cout === 0) 
         else   $error("Failed case not 1011");
-        #50;
 		  
 		  a = 4'b0111;
         sel = 2; 
+        #50;
 		  assert (result === 4'b1000 & cout === 0) 
         else   $error("Failed case not 0111");
-        #50;
 
 		  //---and---
         a = 4'b1010;
         b = 4'b0110;
 		  sel = 3;
+        #50;
 		  assert (result === 4'b0010 & cout === 0) 
         else   $error("Failed case 1010 and 0110");
-        #50;
 		  
         a = 4'b1110;
         b = 4'b0110;
 		  sel = 3;
+        #50;
 		  assert (result === 4'b0110 & cout === 0) 
         else   $error("Failed case 1110 and 0110");
-        #50;
 
 		  //---or---
         a = 4'b1010;
         b = 4'b0110;
         sel = 4; 
+        #50;
 		  assert (result === 4'b1110 & cout === 0) 
         else   $error("Failed case 1010 or 0110");
-        #50;
 		  
 		  a = 4'b1110;
         b = 4'b0110;
         sel = 4; 
+        #50;
 		  assert (result === 4'b1110 & cout === 0) 
         else   $error("Failed case 1110 or 0110");
-        #50;
 
 		  //---xor---
         a = 4'b1010;
         b = 4'b0110;
         sel = 5; 
+        #50;
 		  assert (result === 4'b1100 & cout === 0) 
         else   $error("Failed case 1010 xor 0110");
-        #50;
 		  
 		  a = 4'b1110;
         b = 4'b0110;
         sel = 5; 
+        #50;
 		  assert (result === 4'b1000 & cout === 0) 
         else   $error("Failed case 1110 xor 0110");
-        #50;
 
 		  //---Shift left art
         a = 4'b1010;
         sel = 6;
+        #50;
 		  assert (result === 4'b0100 & cout === 0) 
         else   $error("Failed case shift left art 1010");
-        #50;
 
 		  a = 4'b1110;
         sel = 6; 
+        #50;
 		  assert (result === 4'b1100 & cout === 0) 
         else   $error("Failed case shift left art 1110");
-        #50;
 		  
 		  //---shift left---
         a = 4'b1010;
         sel = 7; 
+        #50;
 		  assert (result === 4'b0100 & cout === 0) 
         else   $error("Failed case shift left 1010");
-        #50;
 
 		  a = 4'b1110;
         sel = 6; 
+        #50;
 		  assert (result === 4'b1100 & cout === 0) 
         else   $error("Failed case shift left 1110");
-        #50;
 		  
 		  //---shift right arit---
         a = 4'b1010;
         sel = 8; 
+        #50;
 		  assert (result === 4'b1101 & cout === 0) 
         else   $error("Failed case shift right art 1010");
-        #50;
 		  
-		  a = 4'b1110;
-        sel = 6; 
+		a = 4'b1110;
+        sel = 8; 
+        #50;
 		  assert (result === 4'b1111 & cout === 0) 
         else   $error("Failed case shift right art 1110");
-        #50;
 
 		  //---shift right---
         a = 4'b1010;
         sel = 9; 
+        #50;
 		  assert (result === 4'b0101 & cout === 0) 
         else   $error("Failed case shift right 1010");
-        #50;
 		  
 		  a = 4'b1110;
-        sel = 6; 
+        sel = 9; 
+        #50;
 		  assert (result === 4'b0111 & cout === 0) 
         else   $error("Failed case shift right 1110");
-        #50;
     
     
     
